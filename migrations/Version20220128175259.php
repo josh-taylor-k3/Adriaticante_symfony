974c2cf4a7cd0e248831ec4b8c49b9da9cb9c324
josh-taylor-k3 <this14hawk@gmail.com>
2022-01-28 23:07:22 +0100

    Fixtures Final

---
+++ migrations/Version20220128175259.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220128175259 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property ADD features_id INT NOT NULL, ADD type_id INT NOT NULL, ADD status_id INT NOT NULL, ADD address_id INT DEFAULT NULL, ADD coordinates_id INT DEFAULT NULL, ADD files_id INT NOT NULL, ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDECEC89005 FOREIGN KEY (features_id) REFERENCES feature (id)');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDEC54C8C93 FOREIGN KEY (type_id) REFERENCES type (id)');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDE6BF700BD FOREIGN KEY (status_id) REFERENCES status (id)');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDEF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDE158B0682 FOREIGN KEY (coordinates_id) REFERENCES coordinates (id)');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDEA3E65B2F FOREIGN KEY (files_id) REFERENCES file (id)');
+        $this->addSql('ALTER TABLE property ADD CONSTRAINT FK_8BF21CDEA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8BF21CDECEC89005 ON property (features_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8BF21CDEC54C8C93 ON property (type_id)');
+        $this->addSql('CREATE INDEX IDX_8BF21CDE6BF700BD ON property (status_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8BF21CDEF5B7AF75 ON property (address_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8BF21CDE158B0682 ON property (coordinates_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8BF21CDEA3E65B2F ON property (files_id)');
+        $this->addSql('CREATE INDEX IDX_8BF21CDEA76ED395 ON property (user_id)');
+        $this->addSql('ALTER TABLE user ADD address_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649F5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D649F5B7AF75 ON user (address_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDECEC89005');
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDEC54C8C93');
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDE6BF700BD');
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDEF5B7AF75');
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDE158B0682');
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDEA3E65B2F');
+        $this->addSql('ALTER TABLE property DROP FOREIGN KEY FK_8BF21CDEA76ED395');
+        $this->addSql('DROP INDEX UNIQ_8BF21CDECEC89005 ON property');
+        $this->addSql('DROP INDEX UNIQ_8BF21CDEC54C8C93 ON property');
+        $this->addSql('DROP INDEX IDX_8BF21CDE6BF700BD ON property');
+        $this->addSql('DROP INDEX UNIQ_8BF21CDEF5B7AF75 ON property');
+        $this->addSql('DROP INDEX UNIQ_8BF21CDE158B0682 ON property');
+        $this->addSql('DROP INDEX UNIQ_8BF21CDEA3E65B2F ON property');
+        $this->addSql('DROP INDEX IDX_8BF21CDEA76ED395 ON property');
+        $this->addSql('ALTER TABLE property DROP features_id, DROP type_id, DROP status_id, DROP address_id, DROP coordinates_id, DROP files_id, DROP user_id');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649F5B7AF75');
+        $this->addSql('DROP INDEX IDX_8D93D649F5B7AF75 ON user');
+        $this->addSql('ALTER TABLE user DROP address_id');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Address;
+use App\Entity\Feature;
+use App\Entity\File;
+use App\Entity\Property;
+use App\Entity\Status;
+use App\Entity\Type;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -21,22 +27,114 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
+
+
+        // ADDRESS
         for ($i = 0; $i < 10; $i++)
         {
+            $address = new Address();
+
+            $address->setStreetNumber($faker->numberBetween(1, 99))
+                    ->setStreetAddressLine1($faker->streetAddress)
+                    ->setCity($faker->city())
+                    ->setStateZipCode($faker->numberBetween(100, 950) . '00')
+                    ->setCountry($faker->country())
+                    ->setPhone('0' . $faker->numberBetween(600000001, 799999999));
+
+            $manager->persist($address);
+
+            //USER
             $user = new User();
-            $user->setEmail('user' . $i . '@test.com')
-                ->setUsername($faker->userName())
-                ->setLastname($faker->lastName())
-                ->setFirstname($faker->firstName())
-                ->setCompany($faker->company());
+            $user->setEmail('user' . $i .'@test.com')
+                    ->setUsername($faker->userName())
+                    ->setLastname($faker->lastName())
+                    ->setFirstname($faker->firstName())
+                    ->setCompany($faker->company())
+                    ->setAddress($address);
 
             $password = $this->encoder->encodePassword($user, 'password');
             $user->setPassword($password);
 
             $manager->persist($user);
+
         }
+        // STATUS
+
+        $status1 = new Status();
+        $status1->setName('Available');
+        $manager->persist($status1);
 
 
+        // TYPE
+
+        $type1 = new Type();
+        $type1->setName('Flat');
+        $manager->persist($type1);
+
+
+
+        // PROPERTY + ADDRESS
+        for ($k = 0; $k < 10; $k++)
+        {
+            // ADDRESS
+            $addressProperty = new Address();
+
+            $addressProperty->setStreetNumber($faker->numberBetween(1, 99))
+                ->setStreetAddressLine1($faker->streetAddress)
+                ->setCity($faker->city())
+                ->setStateZipCode($faker->numberBetween(100, 950) . '00')
+                ->setCountry($faker->country())
+                ->setPhone('0' . $faker->numberBetween(600000001, 799999999));
+
+            $manager->persist($addressProperty);
+
+            //USER
+            $userProperty = new User();
+            $userProperty->setEmail('user1' . $k .'@test.com')
+                ->setUsername($faker->userName())
+                ->setLastname($faker->lastName())
+                ->setFirstname($faker->firstName())
+                ->setCompany($faker->company())
+                ->setAddress($addressProperty);
+
+            $password = $this->encoder->encodePassword($userProperty, 'password');
+            $userProperty->setPassword($password);
+
+            $manager->persist($userProperty);
+
+            // FEATURE
+
+            $feature = new Feature();
+            $feature->setFeature1($faker->word());
+            $manager->persist($feature);
+
+            // FILE
+
+            $file = new File();
+            $file->setFile1('./img/img/adriatic1/jpg');
+            $file->setFile2('./img/img/adriatic1/jpg');
+            $file->setFile3('./img/img/adriatic1/jpg');
+            $manager->persist($file);
+
+            // PROPERTY
+
+            $property = new Property();
+
+            $property->setPrice($faker->numberBetween(40000, 10000000))
+                ->setDescription($faker->text(500))
+                ->setArea($faker->numberBetween(10, 500))
+                ->setTotalRooms($faker->numberBetween(1, 10))
+                ->setTotalBedrooms($faker->numberBetween(0, 8))
+                ->setTotalBathrooms($faker->numberBetween(1, 4))
+                ->setFeatures($feature)
+                ->setType($type1)
+                ->setStatus($status1)
+                ->setAddress($addressProperty)
+                ->setFiles($file)
+                ->setUser($userProperty);
+
+            $manager->persist($property);
+        }
 
         $manager->flush();
     }
