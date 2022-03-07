e415e5ebe9a005b2508b4209f4e595c0f7fd7a6b
josh-taylor-k3 <this14hawk@gmail.com>
2022-03-07 21:25:20 +0100

    Release : remember me + forgot password + templates twig

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
