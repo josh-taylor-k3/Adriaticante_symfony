1c9348788bc947c19eb5dfc2bccb2a9c806e1a79
josh-taylor-k3 <this14hawk@gmail.com>
2022-03-24 19:14:08 +0100

    fix : yarn -> NPM Webpack Ok

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.scss in this case)
+// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
@@ -16,9 +16,7 @@ import './bootstrap';
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all'
 
-
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
-
